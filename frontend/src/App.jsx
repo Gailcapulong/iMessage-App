bce9ba46b505dd4edcd1b92c5c1f1b4881a052b6
Gailcapulong <gailcapulong77@gmail.com>
2026-06-21 22:54:25 +0800

    feat: enhance application with axios integration, real-time socket support, and loading states
    - Added axios instance for API requests.
    - Implemented socket.io for real-time user status tracking.
    - Introduced PageLoader component for better loading experience.
    - Updated ChatPage to include toast notifications.
    - Created useAuthStore for managing authentication state and socket connections.
    - Added utility function for formatting message timestamps.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,11 +4,31 @@ import { Navigate, Route, Routes } from "react-router";
 import ChatPage from "./pages/ChatPage";
 import AuthPage from "./pages/AuthPage";
 import { useAuth } from "@clerk/react";
+import { useAuthStore } from "./store/useAuthStore";
+import { useEffect } from "react";
+
+import { Toaster } from "react-hot-toast";
+import PageLoader from "./components/PageLoader";
 
 function App() {
   const { isSignedIn, isLoaded } = useAuth();
+
+  // option 2 - better for performance
+  const clearAuth = useAuthStore((state) => state.clearAuth);
+  const checkAuth = useAuthStore((state) => state.checkAuth);
+  const isCheckingAuth = useAuthStore((state) => state.isCheckingAuth);
+
+  useEffect(() => {
+    if (!isLoaded) return;
+
+    if (isSignedIn) checkAuth();
+    else clearAuth();
+  }, [checkAuth, clearAuth, isLoaded, isSignedIn]);
+
+  if (!isLoaded || (isSignedIn && isCheckingAuth)) return <PageLoader />;
+
   // todo: make better component
-  if (!isLoaded) return <p>...loading</p>;
+  if (!isLoaded) return <PageLoader />;
 
   return (
     <ThemeProvider>
@@ -26,6 +46,7 @@ function App() {
             element={!isSignedIn ? <AuthPage /> : <Navigate to={"/"} replace />}
           />
         </Routes>
+        <Toaster />
       </WallpaperProvider>
     </ThemeProvider>
   );
